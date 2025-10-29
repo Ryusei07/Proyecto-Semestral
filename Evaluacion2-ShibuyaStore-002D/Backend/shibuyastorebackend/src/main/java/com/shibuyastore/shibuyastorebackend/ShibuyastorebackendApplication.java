894af21a881a576164efcf6713e29e0b3f5d043e
Ryusei07 <diegoignaciozamora@gmail.com>
2025-10-28 23:46:14 -0300

    Creacion de la carpeta Evaluacion2
    El contenido de la carpeta de evaluacion 2, son los dos proyectos, Backend y Frontend, donde por ahora son nuevos (vacios)

---
+++ Evaluacion2-ShibuyaStore-002D/Backend/shibuyastorebackend/src/main/java/com/shibuyastore/shibuyastorebackend/ShibuyastorebackendApplication.java
@@ -0,0 +1,13 @@
+package com.shibuyastore.shibuyastorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShibuyastorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShibuyastorebackendApplication.class, args);
+	}
+
+}
---
+++ Evaluacion2-ShibuyaStore-002D/Backend/shibuyastorebackend/src/test/java/com/shibuyastore/shibuyastorebackend/ShibuyastorebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shibuyastore.shibuyastorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShibuyastorebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
